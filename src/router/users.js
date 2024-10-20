715a5eb80588c52702a27c2f0e226d441fb88f2b
Md. Tanvir Hossain <151984579+tanvirhossain808@users.noreply.github.com>
2024-10-21 00:42:38 +0600

    fixign a bugs

--- src/router/users.js
+++ src/router/users.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const User = require("../models/users")
+const isUserAutheTicate = require("../middlewares/userAuthenticate")
 
 const router = express.Router()
 
-router.get("/", async (req, res) => {
-    const user = await User.find({}).select("email")
-    res.json(user)
+router.get("/uganda", isUserAutheTicate, async (req, res) => {
+    res.json(req.user)
+    // const user = await User.find({}).select("email")
+    // res.json(user)
 })
 
 module.exports = router
